6a9497647ab022f927e3852bef0712ec0b3ad335
kavinda-accelr <kavinda.ravishan@accelr.site>
2023-02-21 11:07:32 +0530

    updated

--- run.bash
+++ run.bash
@@ -2,5 +2,5 @@
 
 g++ ./client/main_tcp_linux.cpp -o ./client/main_tcp_linux.out
 g++ ./server/main_tcp_linux.cpp -o ./server/main_tcp_linux.out
-./client/main_tcp_linux.out &
 ./server/main_tcp_linux.out &
+./client/main_tcp_linux.out &
